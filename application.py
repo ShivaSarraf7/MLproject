18062683522998b1138a38952cc924d396632305
ShivaSarraf7 <sarraf1405@gmail.com>
2026-06-29 20:37:38 +0530

    Update on Application

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
@@ -26,12 +28,15 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+
 
         )
         pred_df=data.get_data_as_data_frame()
+        print("=== DataFrame created from input ===")
         print(pred_df)
+        print("===================================")
         print("Before Prediction")
         predict_pipeline=PredictPipeline()
         print("Mid Prediction")
@@ -41,4 +46,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
