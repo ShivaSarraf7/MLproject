8a1d2fc1bd0ab3e8807ab9b9fcfe3892e35e9d3b
ShivaSarraf7 <sarraf1405@gmail.com>
2026-06-29 20:20:34 +0530

    Making of application and Prediction pipeline.

--- application.py
+++ application.py
@@ -1,27 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
-import logging
 
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application = Flask(__name__)
+application=Flask(__name__)
+
+app=application
 
 ## Route for a home page
 
-@application.route('/')
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
-@application.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        application.logger.info("HERE IS GET")
+    if request.method=='GET':
         return render_template('home.html')
     else:
-        application.logger.info("HERE IS POST")
-        data = CustomData(
+        data=CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
@@ -29,21 +28,17 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
+
         )
-        pred_df = data.get_data_as_dataframe()
-        application.logger.info("HERE IS PREDICTION DATA FRAME")
-        application.logger.info(pred_df.iloc[0])
-        application.logger.info("NOW CREATING PREDICT PIPELINE")
-        predict_pipeline = PredictPipeline()
-        application.logger.info("NOW PREDICTING")
-        results = predict_pipeline.predict(pred_df)
-        application.logger.info("RESULTS ARE HERE:")
-        application.logger.info(results)
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
         return render_template('home.html',results=results[0])
     
-if __name__ == "__main__":
-    application.run(host="0.0.0.0",debug=True,port=80)
-else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    application.logger.handlers = gunicorn_logger.handlers
-    application.logger.setLevel(logging.INFO)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)
